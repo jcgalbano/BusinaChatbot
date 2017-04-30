5a9e671e9a78bea7ca3d6dd14c540eb15bede519
Jan Carlo <jcgalbano@gmail.com>
2017-05-01 04:24:58 +0800

    Attempt to echo

--- chat_bot/views.py
+++ chat_bot/views.py
@@ -13,18 +13,22 @@ class ChatbotView(generic.View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return generic.View.dispatch(self, request, *args, **kwargs)
-    
+
+    # Post function to handle Facebook messages
     def post(self, request, *args, **kwargs):
-
+        # Converts the text payload into a python dictionary
         incoming_message = json.loads(self.request.body.decode('utf-8'))
-
+        # Facebook recommends going through every entry since they might send
+        # multiple messages in a single call during high load
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
-
+                # Check to make sure the received call is a message call
+                # This might be delivery, optin, postback for other events 
                 if 'message' in message:
-                    pprint(message)
-                    post_facebook_message(message['sender']['id'], message['message']['text'])
+                    # Print the message to the terminal
+                    pprint(message) 
 
+                    post_facebook_message(message['sender']['id'], message['message']['text'])    
         return HttpResponse()
 
 def post_facebook_message(fbid, recevied_message):           
