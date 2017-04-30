58f36d05afd1b7b72e7ed77414ede10ffbf396b7
Jan Carlo <jcgalbano@gmail.com>
2017-05-01 04:07:53 +0800

    Handle challenge tokens

--- chat_bot/views.py
+++ chat_bot/views.py
@@ -5,4 +5,7 @@ from django.http.response import HttpResponse
 
 class ChatbotView(generic.View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello World!")
+        if self.request.GET['hub.verify_token'] == '171717':
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
